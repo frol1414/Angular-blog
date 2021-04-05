0ce80dccf84d86d7f01faaa5f96a5b1f675badd3
frol1414 <frol1414@gmail.com>
2021-04-05 10:06:41 +0300

    add post create

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
 import {User} from '../../shared/interfaces'
 import {AuthService} from '../shared/services/auth.service'
-import {Router} from '@angular/router'
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -14,13 +14,21 @@ export class LoginPageComponent implements OnInit {
   // @ts-ignore
   form: FormGroup
   submitted = false
+  message: string | undefined
 
   constructor(
-    private auth: AuthService,
-    private router: Router
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params.loginAgain) {
+        this.message = 'Please, fill the data'
+      }
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
@@ -46,6 +54,8 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
+      this.submitted = false
+    }, () => {
       this.submitted = false
     })
   }
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,12 +1,15 @@
 import {Injectable} from '@angular/core'
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpErrorResponse} from '@angular/common/http'
 import {FbAuthResponse, User} from '../../../shared/interfaces'
-import {Observable} from 'rxjs'
+import {Observable, Subject, throwError} from 'rxjs'
 import {environment} from '../../../../environments/environment'
-import {tap} from 'rxjs/operators'
+import {catchError, tap} from 'rxjs/operators'
 
 @Injectable()
 export class AuthService {
+
+  public error$: Subject<string> = new Subject<string>()
+
   constructor(private http: HttpClient) {}
 
   get token(): string | null {
@@ -24,7 +27,8 @@ export class AuthService {
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         // @ts-ignore
-        tap(this.setToken)
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
       )
   }
 
@@ -36,9 +40,27 @@ export class AuthService {
     return !!this.token
   }
 
+  // tslint:disable-next-line:typedef
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Wrong email')
+        break
+      case 'INVALID_PASSWORD':
+        this.error$.next('Wrong password')
+        break
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Email not found')
+        break
+    }
+    return throwError(error)
+  }
+
   private setToken(response: FbAuthResponse | null): void {
     if (response) {
-      const expDate = new Date(new Date().getDate() + +response.expiresIn * 1000)
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000)
       localStorage.setItem('fb-token', response.idToken)
       localStorage.setItem('fb-token-exp', expDate.toString())
     } else {
